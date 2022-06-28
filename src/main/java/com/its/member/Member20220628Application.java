a7100efcaa57cdffd00f56d25a3fe17bbf8a0f45
AhnSangjun <sdjrdj@naver.com>
2022-06-28 17:20:10 +0900

    회원제게시판 만드는중

---
+++ src/main/java/com/its/member/Member20220628Application.java
@@ -0,0 +1,13 @@
+package com.its.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Member20220628Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Member20220628Application.class, args);
+    }
+
+}
